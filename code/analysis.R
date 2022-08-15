6c298f6227f20a9295a1a024b9972d04de67f26e
jsburns13 <86275366+jsburns13@users.noreply.github.com>
2022-08-15 16:02:07 -0700

    Finished data manipualtion/cleanup, ran first complete RD models

--- code/analysis.R
+++ code/analysis.R
@@ -1,4 +1,33 @@
-### Graph it!
+library(rdrobust)
 
-ggplot(data = df3, mapping = aes(x=time_to_gc, y=log_change, group=tic)) +
-  geom_line()
+### Create feols models and Regression Discontinuity models for min and max events
+
+max_model1 <- feols(normalized ~ date * gc_pre_post, data = max_df_full) %>%
+  etable()
+
+max_model_1rd <- rdrobust(max_df_full$normalized, max_df_full$time_to_maxgc, c=0, p=1, h=75, kernel="uniform") %>%
+  summary()
+
+max_model_2rd <- rdrobust(max_df_full$normalized_2, max_df_full$time_to_maxgc, c=0, p=1, h=75, kernel="uniform") %>%
+  summary()
+
+rdplot(max_df_full$normalized, max_df_full$time_to_maxgc, c=0, p=1, h=75, kernel="uniform", nbins=1000)
+
+rdplot(max_df_full$normalized_2, max_df_full$time_to_maxgc, c=0, p=1, h=75, kernel="uniform", nbins=1000)
+
+rdplot(max_df_full$pct_change, max_df_full$time_to_maxgc, c=0, p=1, h=75, kernel="uniform", nbins=1000)
+
+min_model1 <- feols(normalized ~ date * gc_pre_post, data = min_df_full) %>%
+  etable()
+
+min_model_1rd <- rdrobust(min_df_full$normalized, min_df_full$time_to_mingc, c=0, p=1, h=75, kernel="uniform") %>%
+  summary()
+
+min_model_2rd <- rdrobust(min_df_full$normalized_2, min_df_full$time_to_mingc, c=0, p=1, h=75, kernel="uniform") %>%
+  summary()
+
+rdplot(min_df_full$normalized, min_df_full$time_to_mingc, c=0, p=1, h=75, kernel="uniform", nbins=1000)
+
+rdplot(min_df_full$normalized_2, min_df_full$time_to_mingc, c=0, p=1, h=75, kernel="uniform", nbins=1000)
+
+rdplot(min_df_full$pct_change, min_df_full$time_to_mingc, c=0, p=1, h=75, kernel="uniform", nbins=1000)
--- code/read_and_clean_data.R
+++ code/read_and_clean_data.R
@@ -3,9 +3,11 @@ library(fixest)
 library(vtable)
 library(zoo)
 
-df <- read_csv("data/daily_stonks.csv")
+df <- read_csv("data/daily_stonks_NYSE_2010_2019.csv")
 
 ### Clean data; pick only USD stocks on NYSE; filter to [2010, 2019]
+# This code section was created to pare down a larger dataset; it is no longer
+# needed (unless you query the full dataset again)
 
 df <- df %>%
   filter(curcdd == "USD") %>%
@@ -15,6 +17,10 @@ df <- df %>%
   filter(date >= ISOdate(2010,01,01)) %>%
   filter(date < ISOdate(2020,01,01))
 
+### Save as .csv for easier file size sharing - commented out for reasons
+
+# write.csv(df,"data/daily_stonks_NYSE_2010_2019.csv")
+  
 ### Create rolling averages
 
 df <- df %>%
@@ -52,9 +58,21 @@ df <- df %>%
     dc ==1 ~ date
   ))
 
-### Find first gc date and then join to main df
+### Find max gc date and then join to main df
+### Choosing max to make sure the stocks have been listed for some time
+### and avoid situations where something crosses the golden threshold
+### very early; might be a source of variability
 
-df2 <- df %>%
+max_df <- df %>%
+  filter(is.na(gc_date) == FALSE) %>%
+  group_by(tic) %>%
+  filter(gc_date == max(gc_date)) %>%
+  select(tic, gc_date) %>%
+  rename(last_gc = gc_date)
+
+### We'll also find the min (first) gc to compare as well
+
+min_df <- df %>%
   filter(is.na(gc_date) == FALSE) %>%
   group_by(tic) %>%
   filter(gc_date == min(gc_date)) %>%
@@ -62,24 +80,122 @@ df2 <- df %>%
   rename(first_gc = gc_date)
 
 df <- df %>%
-  inner_join(df2)
+  inner_join(max_df) %>%
+  inner_join(min_df)
 
-### Find +/- 120 days from gc
+### Summary table for info
 
-df3 <- df %>%
-  mutate(time_to_gc = difftime(first_gc, date, units = "days")) %>%
-  filter(time_to_gc >= -120 & time_to_gc <= 120) %>%
-  mutate(time_to_gc = as.numeric(time_to_gc, units="days"))
+df %>%
+  group_by(exchg) %>%
+  summarise(
+    ticker_count = n_distinct(tic)
+    ,mean(first_gc)
+    ,mean(last_gc)
+    ,min(prccd)
+    ,mean(prccd)
+    ,max(prccd)
+            )
+
+### Find +/- 120 days from last gc
+
+max_df_full <- df %>%
+  mutate(time_to_maxgc = -difftime(last_gc, date, units = "days")) %>%
+  filter(time_to_maxgc >= -120 & time_to_maxgc <= 120) %>%
+  mutate(time_to_maxgc = as.numeric(time_to_maxgc, units="days"))
 
 ### Create normalized stock prices
 
-df3 <- df3 %>%
+max_df_full <- max_df_full %>%
   mutate(logprice = log(prccd)) %>%
   arrange(date) %>%
   group_by(tic) %>%
   mutate(pct_change=(prccd/lag(prccd)-1)*100) %>%
-  mutate(log_change=(logprice/lag(logprice)-1)*100)
-
-df2 %>%
+  replace_na(list(pct_change = 1)) %>%
+  mutate(log_change=(logprice/lag(logprice)-1)*100) %>%
+  mutate(gc_pre_post = case_when(
+    time_to_maxgc >= 0 ~ 1,
+    time_to_maxgc < 0 ~ 0
+  )) %>%
+  mutate(normalized=case_when(
+    time_to_maxgc == 0 ~ 1,
+    time_to_maxgc > 0 ~ cumprod(case_when(
+      time_to_maxgc > 0 ~ 1 + (pct_change/100),
+      TRUE ~ 1
+    ))
+  )) %>%
+  arrange(desc(date)) %>%
   group_by(tic) %>%
-  summarize(length(unique(GVKEY)), mean(prccd), min(gc_date), max(gc_date))
+  mutate(normalized = case_when(
+    is.na(normalized) & time_to_maxgc < 0 ~ 1/(
+      cumprod(
+        case_when(
+          time_to_maxgc <= 0 ~ 1 + (
+            pct_change/100
+            ),
+          TRUE ~ 1
+          )
+        )/(
+          1 + (pct_change/100)
+          )
+      ),
+    TRUE ~ normalized
+    )
+    ) %>%
+  arrange(date) %>%
+  group_by(tic) %>%
+  mutate(normalized_2 = case_when(
+    time_to_maxgc == min(time_to_maxgc) ~ 1,
+    TRUE ~ cumprod(1+(pct_change/100))
+  ))
+
+### Find +/- 120 days from first gc
+
+min_df_full <- df %>%
+  mutate(time_to_mingc = -difftime(first_gc, date, units = "days")) %>%
+  filter(time_to_mingc >= -120 & time_to_mingc <= 120) %>%
+  mutate(time_to_mingc = as.numeric(time_to_mingc, units="days"))
+
+### Create normalized stock prices
+
+min_df_full <- min_df_full %>%
+  mutate(logprice = log(prccd)) %>%
+  arrange(date) %>%
+  group_by(tic) %>%
+  mutate(pct_change=(prccd/lag(prccd)-1)*100) %>%
+  replace_na(list(pct_change = 1)) %>%
+  mutate(log_change=(logprice/lag(logprice)-1)*100) %>%
+  mutate(gc_pre_post = case_when(
+    time_to_mingc >= 0 ~ 1,
+    time_to_mingc < 0 ~ 0
+  )) %>%
+  mutate(normalized=case_when(
+    time_to_mingc == 0 ~ 1,
+    time_to_mingc > 0 ~ cumprod(case_when(
+      time_to_mingc > 0 ~ 1 + (pct_change/100),
+      TRUE ~ 1
+    ))
+  )) %>%
+  arrange(desc(date)) %>%
+  group_by(tic) %>%
+  mutate(normalized = case_when(
+    is.na(normalized) & time_to_mingc < 0 ~ 1/(
+      cumprod(
+        case_when(
+          time_to_mingc <= 0 ~ 1 + (
+            pct_change/100
+          ),
+          TRUE ~ 1
+        )
+      )/(
+        1 + (pct_change/100)
+      )
+    ),
+    TRUE ~ normalized
+  )
+  ) %>%
+  arrange(date) %>%
+  group_by(tic) %>%
+  mutate(normalized_2 = case_when(
+    time_to_mingc == min(time_to_mingc) ~ 1,
+    TRUE ~ cumprod(1+(pct_change/100))
+  ))
